c926faa6571ca05df4a3f7d2dc5ba7156b11722c
Sergiogil5 <sergiogil.09@gmail.com>
2025-06-26 07:49:03 +0200

    Modificacion RegistroRequest

--- src/main/java/com/entrenamiento/planverano/user/RegistroRequest.java
+++ src/main/java/com/entrenamiento/planverano/user/RegistroRequest.java
@@ -9,13 +9,13 @@ public class RegistroRequest {
 
     // Getters y Setters
     public String getCodigoRegistro() { return codigoRegistro; }
-    public void setCodigoRegistro(String c) { this.codigoRegistro = c; }
+    public void setCodigoRegistro(String codigoRegistro) { this.codigoRegistro = codigoRegistro; }
     public String getEmail() { return email; }
-    public void setEmail(String e) { this.email = e; }
+    public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
-    public void setPassword(String p) { this.password = p; }
+    public void setPassword(String password) { this.password = password; }
     public String getNombreCompleto() { return nombreCompleto; }
-    public void setNombreCompleto(String n) { this.nombreCompleto = n; }
+    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
     public Categoria getTeam() { return team; }
-    public void setTeam(Categoria t) { this.team = t; }
+    public void setTeam(Categoria team) { this.team = team; }
 }
