bcfa4e0469895247f2a209f4866bf06429b313ca
Sergiogil5 <sergiogil.09@gmail.com>
2025-06-26 06:55:41 +0200

    Modificacion RegistroRequest

--- src/main/java/com/entrenamiento/planverano/user/RegistroRequest.java
+++ src/main/java/com/entrenamiento/planverano/user/RegistroRequest.java
@@ -1,31 +1,21 @@
 package com.entrenamiento.planverano.user;
 
-// Ya no necesitamos la importación de @JsonProperty, la eliminamos.
-
 public class RegistroRequest {
     private String codigoRegistro;
     private String email;
     private String password;
     private String nombreCompleto;
+    private Categoria team; // Asegurémonos de que el nombre es 'team'
 
-    // --- ¡CAMBIO 1! ---
-    private Categoria team; // El campo ahora se llama 'team'
-
-    // --- Getters ---
+    // Getters y Setters
     public String getCodigoRegistro() { return codigoRegistro; }
+    public void setCodigoRegistro(String c) { this.codigoRegistro = c; }
     public String getEmail() { return email; }
+    public void setEmail(String e) { this.email = e; }
     public String getPassword() { return password; }
+    public void setPassword(String p) { this.password = p; }
     public String getNombreCompleto() { return nombreCompleto; }
-
-    // --- ¡CAMBIO 2! ---
-    public Categoria getTeam() { return team; } // Nuevo Getter para 'team'
-
-    // --- Setters ---
-    public void setCodigoRegistro(String codigoRegistro) { this.codigoRegistro = codigoRegistro; }
-    public void setEmail(String email) { this.email = email; }
-    public void setPassword(String password) { this.password = password; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
-
-    // --- ¡CAMBIO 3! ---
-    public void setTeam(Categoria team) { this.team = team; } // Nuevo Setter para 'team'
+    public void setNombreCompleto(String n) { this.nombreCompleto = n; }
+    public Categoria getTeam() { return team; }
+    public void setTeam(Categoria t) { this.team = t; }
 }
