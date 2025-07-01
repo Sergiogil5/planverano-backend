b8bcdbb95fec2e9c063336065106bb38c18a6267
Sergiogil5 <sergiogil.09@gmail.com>
2025-07-01 22:53:45 +0200

    Base de datos

--- src/main/java/com/entrenamiento/planverano/training/ProgresoJugador.java
+++ src/main/java/com/entrenamiento/planverano/training/ProgresoJugador.java
@@ -16,7 +16,7 @@ public class ProgresoJugador {
     private User usuario;
 
     // Relación con la sesión que completó
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER) // <-- ¡AQUÍ ESTÁ EL CAMBIO!
     @JoinColumn(name = "sesion_diaria_id", nullable = false)
     private SesionDiaria sesion;
 
